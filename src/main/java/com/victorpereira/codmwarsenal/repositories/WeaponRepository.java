53a7542a9498312dc3311296011e706e5c881b10
Victor Pereira <victor.cordeiro2@gmail.com>
2020-08-16 19:49:14 -0300

    Query by name and by type

--- src/main/java/com/victorpereira/codmwarsenal/repositories/WeaponRepository.java
+++ src/main/java/com/victorpereira/codmwarsenal/repositories/WeaponRepository.java
@@ -1,9 +1,14 @@
 package com.victorpereira.codmwarsenal.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.victorpereira.codmwarsenal.models.Weapon;
 
 public interface WeaponRepository extends MongoRepository<Weapon, String> {
 
+	Weapon findByNameContainingIgnoreCase(String name);
+	
+	List<Weapon> findByTypeIgnoreCase(String type);
 }
--- src/main/java/com/victorpereira/codmwarsenal/resources/WeaponResource.java
+++ src/main/java/com/victorpereira/codmwarsenal/resources/WeaponResource.java
@@ -10,36 +10,50 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.victorpereira.codmwarsenal.models.Weapon;
 import com.victorpereira.codmwarsenal.repositories.WeaponRepository;
+import com.victorpereira.codmwarsenal.resources.utils.URL;
 
 @RestController
-@RequestMapping(value="/weapons")
+@RequestMapping(value = "/weapons")
 public class WeaponResource {
-	
+
 	@Autowired
 	private WeaponRepository repo;
-	
+
 	@GetMapping
 	public List<Weapon> findAll() {
 		return repo.findAll();
 	}
-	
-	@GetMapping(value="/{id}")
+
+	@GetMapping(value = "/{id}")
 	public Weapon findById(@PathVariable String id) {
 		return repo.findById(id).orElseThrow();
 	}
-	
+
+	@GetMapping(value = "/name")
+	public Weapon findByName(@RequestParam(value = "text") String name) {
+		name = URL.decodeParam(name);
+		return repo.findByNameContainingIgnoreCase(name);
+	}
+
+	@GetMapping(value = "/type")
+	public List<Weapon> findByType(@RequestParam(value = "text", defaultValue = "") String type) {
+		type = URL.decodeParam(type);
+		return repo.findByTypeIgnoreCase(type);
+	}
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public Weapon insert(@RequestBody Weapon weapon) {
 		return repo.insert(weapon);
 	}
-	
-	@DeleteMapping(value="/{id}")
+
+	@DeleteMapping(value = "/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable String id) {
 		repo.deleteById(id);
